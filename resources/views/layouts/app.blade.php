bb9df5b4825e5aae0b2e864106de88c5cd361937
Analia Mok <axm2580@g.rit.edu>
2018-03-31 13:00:29 -0400

    Initial templating setup. Created Pages controller and corresponding routes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>{{ config('app.name'), 'LSAPP' }}</title>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  @include('partials/navbar')
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+</html>
