601c3a265430db7b14ccc6568c287f9660933a8b
Analia Mok <axm2580@g.rit.edu>
2018-04-01 10:55:18 -0400

    Initialized Laravel default authentication. Adjusted navbar to account for users.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ config('app.name'), 'LSAPP' }}</title>
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'LSAPP') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  @include('partials/navbar')
-  <div class="container">
-    @include('partials/messages')
-    @yield('content')
-  </div>
-  <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-  <script>
-    CKEDITOR.replace( 'post-ckeditor' );
-  </script>
+    <div id="app">
+      @include('partials/navbar')
+        <main class="container">
+            @include('partials/messages')
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
