306c0b1984d6fe0b49f3f2eadedbdcb51d191624
Analia Mok <axm2580@g.rit.edu>
2018-04-01 10:36:09 -0400

    Added edit and delete functionality to posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts/app')
 
 @section('content')
-  <a href="/posts" class="btn">Go Back</a>
-  <h1>{{$post->title}}</h1>
+  <a href="/posts" class="btn btn-secondary">Go Back</a>
+  <h1 style="margin-top:1rem;">{{$post->title}}</h1>
   <div>
     {!! $post->body !!}
   </div>
-  <hr>
+  <hr style="margin-top:2rem;">
   <small>Written on {{$post->created_at}}</small>
+  <hr>
+  <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a>
+  
+  {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
+    <!-- Spoofing DELETE request -->
+    {{ Form::hidden('_method', 'DELETE') }}
+    {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+  {!! Form::close() !!}
 @endsection
